5af2ea4ffa9f13a61acaafb1b121bf7a99f4c6d9
Donodapadoca <joao.timao@hotmail.com>
2023-01-05 16:59:13 -0300

    player quase render

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,11 @@
 
 import java.awt.Canvas;
 import java.awt.Dimension;
-
+import java.awt.image.BufferStrategy;
+import java.awt.Graphics;
 import javax.management.monitor.GaugeMonitor;
 import javax.swing.JFrame;
+import java.awt.image.BufferedImage;
 
 public class Game extends Canvas implements Runnable {
 
@@ -11,8 +13,13 @@ public class Game extends Canvas implements Runnable {
     public static int HEIGHT = 120;
     public static int SCALE = 2;
 
+    public BufferedImage layer = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+    public Player player;
+
     public Game() {
         this.setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+
+        player = new Player();
     }
 
     public static void main(String[] args) {
@@ -21,16 +28,47 @@ public class Game extends Canvas implements Runnable {
         JFrame frame = new JFrame("Pong");
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
         frame.add(game);
         frame.pack();
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
 
+        new Thread(game).start();
+    }
+
+    public void tick() {
+
+    }
+
+    public void render() {
+        BufferStrategy bs = this.getBufferStrategy();
+        if (bs == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
+        Graphics g = layer.getGraphics();
+        player.render(g);
+
+        g = bs.getDrawGraphics();
+        g.drawImage(layer, 0, 0, WIDTH * SCALE, HEIGHT * SCALE, null);
+
+        bs.show();
     }
 
     @Override
     public void run() {
-        // TODO Auto-generated method stub
+        while (true) {
+            render();
+            tick();
+            try {
+                Thread.sleep(1000 / 60);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                // TODO: handle exception
+            }
+            // TODO Auto-generated method stub
+
+        }
 
     }
 
